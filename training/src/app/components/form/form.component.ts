a6b376c95dd1caab8532802de426b9c8dd336fb3
arunselvakumar <b.arunselvakumar@outlook.com>
2019-05-16 15:33:38 +0530

    Form Changes With Console.log()

--- training/src/app/components/form/form.component.ts
+++ training/src/app/components/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -12,4 +13,7 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
 
+  saveDetails(ngForm: NgForm) {
+    console.log(ngForm.value);
+  }
 }
