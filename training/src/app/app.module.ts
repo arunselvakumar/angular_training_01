9865b7ed1d66f6976ad7a65467098abcd7cc9cd5
arunselvakumar <b.arunselvakumar@outlook.com>
2019-05-16 12:04:05 +0530

    Changes Merged

--- training/src/app/app.module.ts
+++ training/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BookStoreComponent } from './components/book-store/book-store.component';
 import { BookStoreListComponent } from './components/book-store/book-store-list/book-store-list.component';
 import { BookDetailsComponent } from './components/book-store/book-details/book-details.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BookDetailsComponent } from './components/book-store/book-details/book-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- training/src/app/components/book-store/book-store.component.ts
+++ training/src/app/components/book-store/book-store.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookStoreComponent implements OnInit {
 
+  imageUrl: string;
+
   constructor() { }
 
   ngOnInit() {
