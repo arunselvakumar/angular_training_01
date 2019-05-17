eb6f2f002d319c1d1d3761321ab71954290f9a71
arunselvakumar <b.arunselvakumar@outlook.com>
2019-05-17 09:53:36 +0530

    Integrated Forms Control

--- training/src/app/components/data-driven-forms/data-driven-forms.component.ts
+++ training/src/app/components/data-driven-forms/data-driven-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven-forms',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataDrivenFormsComponent implements OnInit {
 
+  formGroup: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.formGroup = new FormGroup({
+      title: new FormControl(),
+      email: new FormControl(),
+      price: new FormControl(),
+      author_name: new FormControl()
+    });
   }
 
 }
