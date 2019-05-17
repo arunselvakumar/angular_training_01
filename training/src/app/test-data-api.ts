bc8c878528854fe0fb80eb9ce2974709af9eb15e
arunselvakumar <b.arunselvakumar@outlook.com>
2019-05-17 15:09:04 +0530

    Service Canges Merged

--- training/src/app/test-data-api.ts
+++ training/src/app/test-data-api.ts
@@ -4,9 +4,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class TestData implements InMemoryDbService {
   createDb() {
     const users = [
-      new UserModel( 'Arun', 'SSE'),
-      new UserModel( 'Selva', 'SSE'),
-      new UserModel( 'Kumar', 'SSE'),
+      new UserModel( 'Name 1', 'SE'),
+      new UserModel( 'Name 2', 'SSE'),
+      new UserModel( 'Name 3', 'PSE'),
     ];
 
     return { users: users };
