3cb7472e8bfd4fd7c035f6ada14fa17a8bfc3ff6
arunselvakumar <b.arunselvakumar@outlook.com>
2019-05-17 12:45:41 +0530

    Form Builder Changes Merged

--- training/src/app/components/state-demo/state-demo.component.ts
+++ training/src/app/components/state-demo/state-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../state/app.state';
 import {BlockchainModel} from '../../models/blockchain.model';
@@ -13,10 +13,10 @@ export class StateDemoComponent implements OnInit {
 
   private currencyFormGroup: FormGroup;
 
-  constructor(private readonly store: Store<AppState>) { }
+  constructor(private readonly store: Store<AppState>, private readonly formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.currencyFormGroup = new FormGroup({
+    this.currencyFormGroup = this.formBuilder.group({
       name: new FormControl(''),
       value: new FormControl('')
     });
@@ -24,6 +24,7 @@ export class StateDemoComponent implements OnInit {
 
   onSubmit() {
     if (this.currencyFormGroup.valid) {
+      console.log(this.currencyFormGroup.value);
       this.store.dispatch({ type: 'ADD_COIN', payload: this.currencyFormGroup.value });
     }
   }
