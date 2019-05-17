0b7759158f743c573c58f0cb21b1d3aba64d82a0
arunselvakumar <b.arunselvakumar@outlook.com>
2019-05-17 10:24:45 +0530

    Integrated Forms Control

--- training/src/app/components/data-driven-forms/data-driven-forms.component.ts
+++ training/src/app/components/data-driven-forms/data-driven-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {Form, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven-forms',
@@ -10,15 +10,42 @@ export class DataDrivenFormsComponent implements OnInit {
 
   formGroup: FormGroup;
 
+  title: FormControl;
+
+  email: FormControl;
+
+  price: FormControl;
+
+  author_name: FormControl;
+
   constructor() { }
 
   ngOnInit() {
+    this.initializeFormControls();
+    this.initializeForm();
+  }
+
+  initializeFormControls() {
+    this.title = new FormControl('', [Validators.required, Validators.maxLength(5)]);
+    this.author_name = new FormControl('', Validators.required);
+    this.price = new FormControl('', Validators.required);
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+  }
+
+  initializeForm() {
     this.formGroup = new FormGroup({
-      title: new FormControl(),
-      email: new FormControl(),
-      price: new FormControl(),
-      author_name: new FormControl()
+      title: this.title,
+      email: this.email,
+      price: this.price,
+      author_name: this.author_name
     });
+  }
+
+  onSubmit() {
+    if (this.formGroup.valid) {
+      console.log(this.formGroup.value);
+      this.formGroup.reset();
+    }
   }
 
 }
